b860842e99342faa36075b6cfbf3cdff95417a55
Nikolay <pavitskiy@yandex.ru>
2022-06-10 17:43:50 +0300

    MyExample 001 Dog project

---
+++ myexample001_Dog/Program.cs
@@ -0,0 +1,21 @@
+﻿float dist = 10000;
+float vd = 5, v1 = 1, v2 = 2;
+float dist1 = 0, dist2 = 0, time = 0;
+int number = 0;
+while (dist > 10)
+{
+    number = number + 1;
+    if((number / 2) == 0)
+    {
+        time = dist / (vd + v2);
+    }    
+    else
+    {
+        time = dist / (vd + v1);
+    }
+    dist1 = v1 * time;
+    dist2 = v2 * time;
+    dist = dist - dist1 - dist2;
+}
+Console.Write("Number=");
+Console.WriteLine(number);
